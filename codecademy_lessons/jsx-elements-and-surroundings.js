d2d67854436c39463aa410c5a688727048d7efda
fusionstar <errolcoombs@PG-LT-1021.local>
2019-02-06 20:27:47 -0500

    day 2 of of 100 days of code

---
+++ codecademy_lessons/jsx-elements-and-surroundings.js
@@ -0,0 +1,15 @@
+/// example of a JSX element being stored in a variable: 
+
+const navBar = <nav>I am a nav bar</nav>;
+
+const myArticle = <article></article>;
+
+/// example of several JSX elements being stored in an object: 
+
+const myTeam = {
+  center: <li> Benzo Walli</li>,
+  powerForward: <li>Rasha Loa</li>,
+  smallForward: <li>Tayshaun Dasmoto</li>,
+  shootingGround: <li>Colmar Cumberbatch</li>,
+  pointGuard: <li>Femi Billion</li>
+}
---
+++ codecademy_lessons/jsx-elements.js
@@ -0,0 +1,3 @@
+//In app.js, write a JSX <p></p> element containing the text, Hello world. Use the example code above as a guide.
+
+<p>Hello World</p>
